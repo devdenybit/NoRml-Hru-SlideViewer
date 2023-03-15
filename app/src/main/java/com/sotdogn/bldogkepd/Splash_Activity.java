09e7e320082eb7786431fcc11676faf91dac19bf
div79 <blackbotinfotech@gmail.com>
2023-03-15 19:41:08 +0530

    version1.0

---
+++ app/src/main/java/com/sotdogn/bldogkepd/Splash_Activity.java
@@ -0,0 +1,19 @@
+package com.sotdogn.bldogkepd;
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+
+public class Splash_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+    }
+
+}
---
+++ vclibs/src/main/java/com/sotdogn/bldogkepd/models/MoreApp.java
@@ -0,0 +1,29 @@
+package com.sotdogn.bldogkepd.models;
+
+
+
+public class MoreApp {
+
+
+    private String name;
+
+    private String url;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+}
